0449cbf6ab31c69b614436433a4f6c1d31c303e7
veegas <mohamed.yasser.abdel.razek@gmail.com>
2016-07-12 01:01:47 +0200

    Updated Tue Jul 12 01:01:47 EET 2016

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-
-gulp build
-time = date
-git add $1 && git commit -m "Updated $time"
-git subtree push --prefix $1 origin gh-pages
+# gulp build
+git add dist && git commit -m "Updated $('date')"
+git subtree push --prefix dist origin gh-pages
+# git subtree push --prefix dist tedx gh-pages
